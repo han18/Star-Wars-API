598127dbc1637fe457bc78d9ced49aefa639464d
han18 <han.fadah@gmail.com>
2024-02-01 13:33:33 -0500

    imported useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // State to hold star wars data
+  const [starData, setStarData] = useState([]);
+
+  // Function to get star wars data api
+  const getStarWars = async (searchItems) => {
+    // Make fetch request and store the response
+    try {
+      const response = await fetch(
+        `https://swapi.dev/api/starships/${searchItems}`
+      );
+      const data = await response.json();
+      setStarData(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return <div className="App">This is the main App</div>;
 }
 
